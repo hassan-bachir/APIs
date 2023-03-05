f9f47ac37110f852ea207ed3c36f5cbc8acdb8c2
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-03-05 23:22:27 +0200

    prime age api

--- returnIP.php
+++ returnIP.php
@@ -4,6 +4,8 @@
 $clientIpAddress = $_SERVER['REMOTE_ADDR'];
 $jsonResponse = json_encode(array("ip_address" => $clientIpAddress));
 header('Content-Type: application/json');
+
+
 echo $jsonResponse;
 
 ?>
